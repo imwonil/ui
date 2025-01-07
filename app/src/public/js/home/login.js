df0e8417cad2769ebab3398d77cf99033354195b
imwonil <120043606+imwonil@users.noreply.github.com>
2025-01-07 19:00:40 +0900

    Update login.js

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -36,7 +36,7 @@ const findExit = document.querySelector(".find-exit")
 
 /**아이디/비번 찾기랑 회원가입 모달 각각 끄는 방법 */
 
-changeModal.classList.toggle("hidden")
+
 /**X 눌렀을 때 아이디/비밀번호 찾기 및 퇴실 창 제거 */
 findExit.addEventListener("click", ()=>{
 
@@ -73,7 +73,7 @@ findAnchor.forEach((ele)=>{
 
 
          findModal.classList.toggle("hidden")
-        
+
 
 
     // idx를 6으로 함으로써 모달의 첫번째 칸에 커서가 들어간다. 
@@ -105,7 +105,7 @@ findAnchor.forEach((ele)=>{
 
 
        if(userNameindex !== userPhoneindex){
-         
+
          warningModalText.innerHTML = "전화번호와 이름이 다릅니다."
           warningModal.classList.toggle("hidden")
 
@@ -118,27 +118,27 @@ findAnchor.forEach((ele)=>{
        }
 
                    else if(!PhoneInclude) {
-                     
+
                        warningModalText.innerHTML = "전화번호가 존재하지 않습니다."
                      warningModal.classList.toggle("hidden")
                      setTimeout(() => {
                       //  sucModal.classList.toggle("hidden")
                       warningModal.classList.toggle("hidden")
-          
+
                    }, 2000); 
                      return
                    }
                         else if((!NameInclude)){
-                          
-                          
-                          
-                        
+
+
+
+
                             warningModalText.innerHTML = "존재하지 않는 이름입니다."
                            warningModal.classList.toggle("hidden")
                            setTimeout(() => {
                             //  sucModal.classList.toggle("hidden")
                             warningModal.classList.toggle("hidden")
-                
+
                          }, 2000);
                         return
                         }
@@ -155,11 +155,11 @@ findAnchor.forEach((ele)=>{
 
 
                  }
-          
+
 
           sucModalText.innerHTML = "인증번호 전송하였습니다."
           sucModal.classList.toggle("hidden")
-                
+
 
 
             fetch("/search", {
@@ -172,16 +172,16 @@ findAnchor.forEach((ele)=>{
               })
               .then((res => res.json()))
               .then((res) => {
-                
+
                 console.log(res)
                 con_bu.addEventListener("click", CONFIRM) 
                 setTimeout(() => {
-                  
+
                   sucModal.classList.toggle("hidden") // 인증번호 전송 하였습니다
-      
+
                }, 2000); 
                 function CONFIRM() {
-                
+
                  if(certification.value === res.search) {
             const C_P = document.querySelector("#changPhone")
             const C_PS = document.querySelector("#changPsword")
@@ -189,7 +189,7 @@ findAnchor.forEach((ele)=>{
             const C_B = document.querySelector("#changButton")
                        changeModal.classList.toggle("hidden")
                        findModal.classList.toggle("hidden")
-                         
+
 
          fetch('/users')
          .then(res => res.json())
@@ -220,9 +220,9 @@ findAnchor.forEach((ele)=>{
     if(C_P.value !== res.phone) { 
     warningModalText.innerHTML = "등록된 전화번호가 다릅니다."
     warningModal.classList.toggle("hidden")
-    
+
     setTimeout(() => {
-      
+
       warningModal.classList.toggle("hidden")
 
    }, 2000);
@@ -231,35 +231,35 @@ findAnchor.forEach((ele)=>{
          if(S_P === false) {
           warningModalText.innerHTML = "전화번호가 다릅니다."
           warningModal.classList.toggle("hidden")
-          
+
           setTimeout(() => {
-            
+
             warningModal.classList.toggle("hidden")
 
          }, 2000);
         return
-            
+
          }
 
-          
+
           // if(C_PS.value !== C_C_P.value) {
-             
+
           //   warningModalText.innerHTML = "비밀번호가 일치하지 않습니다."
           //   warningModal.classList.toggle("hidden")
 
-           
+
           //   setTimeout(() => {
-              
+
           //     warningModal.classList.toggle("hidden")
-  
+
           //  }, 2000);
           // return
-        
+
           // } 
           if(!C_PS.value) { warningModalText.innerHTML = "비밀번호를 설정 하세요."
           warningModal.classList.toggle("hidden") 
           setTimeout(() => {
-              
+
             warningModal.classList.toggle("hidden")
 
          }, 2000);
@@ -280,15 +280,15 @@ findAnchor.forEach((ele)=>{
       .then((res) => {  
 
     if(res.success === true){ 
-      
-    
+
+
       sucModalText.innerHTML = "비밀번호가 수정 되었습니다."
       sucModal.classList.toggle("hidden")
       setTimeout(() => {
            location.href = "/login"
           sucModal.classList.toggle("hidden") // 임시로 해둠 location.href 활성화시 삭제 및 주석 처러함
      }, 2000);
-   
+
 
   }
       })
@@ -305,22 +305,22 @@ findAnchor.forEach((ele)=>{
                   }
 
                   if(certification.value !== res.search) {
-                    
-                    
-                 
-                  
+
+
+
+
 
                       warningModalText.innerHTML = "인증번호가 일치 하지 않습니다."
                        warningModal.classList.toggle("hidden")
                        setTimeout(() => {
-              
+
                         warningModal.classList.toggle("hidden")
-            
+
                      }, 2000);
 
                      return
                   }
-               
+
                 }
 
                })
@@ -369,64 +369,64 @@ function LOGINBUTTON () {
         return new Promise((resolve, reject) => {
           console.log("1")
         if(resolve) {
-            
+
           sucModalText.innerHTML =`${req.phon} 반갑습니다 :)`
           sucModal.classList.toggle("hidden")
           setTimeout(() => {
-             
+
            sucModal.classList.toggle("hidden")
-       
-               
+
+
          }, 1300); 
-        
-         
+
+
             resolve();
-      
+
         }else { reject("err")  } 
-      
-        
+
+
       })
-      
-      
-      
-      
+
+
+
+
       }
       function  next2(){
         return new Promise((resolve, reject) => {
           console.log("1")
         if(resolve) {
-            
+
           setTimeout(() => {
             checkin.classList.toggle("hidden");
-      
+
             // keyboardContainer.classList.toggle("blur")
-    
+
           }, 1200);
-         
+
             resolve();
-      
+
         }else { reject("err")  } 
-      
-        
+
+
       })
-      
-      
-      
-      
+
+
+
+
       }
       next().then(( )  => {return  next2()})  
         //  .then(() =>{ return next2()})
-         
 
-      
+
+
 
       // sucModalText.innerHTML =`${req.phon} 방갑습니다 :)`
       //  sucModal.classList.toggle("hidden")
       //  setTimeout(() => {
-          
+
       //   sucModal.classList.toggle("hidden")
-    
-            
+
+
       // }, 1600); 
 
       /*입실(checkin-anchor)을 눌렀을 때 checkin 모달창 열림 */
@@ -508,9 +508,9 @@ function LOGINBUTTON () {
          span3.innerHTML="미사용"
          span4.innerHTML= "자리선택"
          span4.id= `index${i}`
-         
+
          span5.value ="cad"
-        
+
                  }
                if( Niceinformation[0].cancal[i] === "Y" ) {
                 console.log("1")
@@ -524,7 +524,7 @@ function LOGINBUTTON () {
 
                 span5.innerHTML="상품 취소 불가"
                 span5.id= `index${i}cancel`
-              
+
               }
          if (res.wonset[i] !== ''){
           span3.innerHTML='사용중'
@@ -556,14 +556,14 @@ function LOGINBUTTON () {
 
     }
     else if (res.success === false) {
-    
+
       warningModalText.innerHTML = `${res.msg}`
        warningModal.classList.toggle("hidden")
        setTimeout(() => {
-          
+
         warningModal.classList.toggle("hidden")
-    
-            
+
+
       }, 1600); 
       } 
 
@@ -760,10 +760,10 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
 
 
      const TAB =  ele.innerText
-       
+
     if(TAB === "로그아웃") {
 
-        
+
       logoutButton.addEventListener("click", logOUT) 
 
   function logOUT() {
@@ -791,10 +791,10 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
           if(res.id !== undefined) {
 
             if(res.goodsName.length === "0") {
-              
+
                warningModal.classList.toggle("hidden")
                 warningModalText.innerHTML = "상품이 존재하지 않습니다."
-  
+
 
              location.href = "/login"
           }
@@ -803,7 +803,7 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
             /*입실(checkin-anchor)을 눌렀을 때 checkin 모달창 열림 */
 
               checkin.classList.toggle("hidden");
-        
+
               // keyboardContainer.classList.toggle("blur") //err 남
 
 
@@ -814,7 +814,7 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
              }
 
               var time =  (res.UseTime[i]%1440) //나머지 분
-              
+
 
             var day = Math.floor( res.UseTime[i]/1400) // 1일
             var hour =  Math.floor(time/60) // 시간
@@ -877,14 +877,14 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
                 const text=  document.getElementById(`indexOut${i}`).innerText
                 const indexID =  document.getElementById(`indexOut${i}`).id
                 document.querySelector(`#indexOut${i}`).onclick =() =>{logoutTime(indexID,text)}
-             
+
               }                // 현자 사용하고 있는 여부
 
 
 
 
               span5.innerHTML="상품 취소(환불)"
-             
+
              //  td.innerText =`현재 사용중인 자리 : ${data[0].wonset[i]}  상품:${data[0].goodsName[i]} - ${data[0].benchName[i]} `
 
             }    
@@ -893,7 +893,7 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
 
              warningModal.classList.toggle("hidden")
              warningModalText.innerHTML = "퇴실할 상품이 없습니다."
-              
+
                location.href = "/login"
           }
 
@@ -901,12 +901,12 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
                         warningModalText.innerHTML = `${res.msg}`
                         warningModal.classList.toggle("hidden")
                         setTimeout(() => {
-                          
+
                           warningModal.classList.toggle("hidden")
                           console.log(res.msg)
                           return
                       }, 2000);
-                       
+
  } 
 
                       function logoutTime(indexID,text) {
@@ -939,7 +939,7 @@ tab.forEach((ele)=>{  //tab 클릭시 반응하는 함수
 
                                                    sucModalText.innerHTML = "퇴실 처리 되었습니다."
                                                    sucModal.classList.toggle("hidden")
-                                
+
                                 location.href = "/login"
 
                                  }
@@ -1010,7 +1010,7 @@ function next() {
 
       warningModal.classList.toggle("hidden")
        warningModalText.innerHTML = "상품이 존재 하지 않습니다."
-     
+
        location.href  = "/login"
    }
 
@@ -1031,28 +1031,28 @@ function cardCancel(a,b,phon) {
   const  index= parseInt(result);
   checkin.classList.toggle("hidden");
   if(b === "상품 취소(관리자 승인)") {
-    
+
     document.querySelector(".modal").classList.remove("hidden")
 
 
 
-  
+
    } else if(b === "상품 취소(관리자 문의)") {
 
 
     setTimeout(() => {
-    
+
       alert("관리자에게 문의후 환불 진행주세요")
       location.href = "/login"
    }, 1500);
 return
    } else if(b==="상품 취소 불가") {
     setTimeout(() => {
-    
+
       alert("상품 취소 불가 관리자에게 문의 해주세요")
       location.href = "/login"
    }, 1500);
-   
+
 return
    }
 
@@ -1062,14 +1062,14 @@ return
 .then(datas => { 
   var Nicename = datas.filter(function (addSave) { return addSave.phon === phon });
 
-                   
-         
+
+
          console.log(Nicename[0].approvalDay[index],"승인날짜",  Nicename[0].hangle[index],"할부", Nicename[0].fee[index],"금액" , Nicename[0].approvalNumber[index],"승인번호")
-        
+
           const approvalDAY =Nicename[0].approvalDay[index].substr(0,6)
           console.log(approvalDAY)
-    
-           
+
+
            var TYPE = "VCAT";
            var TYPE2 = "NICEVCAT";
            var FS = '\x1C';
@@ -1077,21 +1077,21 @@ return
            var sendbuf;
            var iFlag = '0';
           //  document.getElementById("feeName").innerText = `취소 금액 :  ${fee}`
-          
+
            const seet = document.getElementById('seet');
-          
-           
-             
-           
-           
+
+
+
+
+
            console.log(document.getElementById("cancel"),"230")
            const  Button = document.getElementById("cancel").addEventListener("click", handleClick('credit_cancel'))
-          
-          
+
+
           function handleClick(myRadio) //카드 결제 information 
           {        
             console.log(myRadio,"myRadiom")
-          
+
             if(myRadio == 'credit_cancel') //신용취소
             {
               // sendbuf = "NICEVCAT" + H7 + "0420" + FS + "10" + FS + "C" + FS +"1000" + FS + "0" + FS + "0" + FS + "00" + FS + "28700883" + FS + "240321" + FS + "" + FS + FS + FS + FS + "0" + FS + FS + FS + FS + "" + FS + H7;
@@ -1099,22 +1099,22 @@ return
               // sendbuf = "NICEVCAT" + H7 + "0420" + FS + "10" + FS + "C" + FS + data[0].fee[index] + FS +"0" + FS + data[0].hangle[index] + FS + data[0].approvalNumber[index] + FS + data[0].approvalNumber[index] + FS + approvalDAY + FS + "" + FS + FS + FS + FS + "0" + FS + FS + FS + FS + "" + FS + H7;
             }
             console.log(sendbuf,"kiiie")
-            
+
             // 		else if(myRadio.value == 'credit_cancel') //신용취소
             // {
             // 	sendbuf = "NICEVCAT" + H7 + "0420" + FS + "10" + FS + "C" + FS + form.money.value + FS + form.tax.value + FS + form.bongsa.value + FS + form.halbu.value + FS + form.agreenum.value + FS + form.agreedate.value + FS + form.catid.value + FS + FS + FS + FS + form.myunse.value + FS + FS + FS + FS + "" + FS + H7;
             // }
-      
-        
-          
+
+
+
             form.SendData.value = sendbuf +""+`${Nicename[0].phon}`+""+`${index}`  ;	 // 이정보를 가지고 post 
             console.log(form.SendData.value, "39")		
-               
+
           }	
-           
-               
+
+
         })
-    
+
 }
 
 var TYPE = "VCAT";
@@ -1127,18 +1127,18 @@ var iFlag = '0';
   { 	
     var sendMsg;	
     var RecvData;
-    
+
     form.RecvData.value = "";
     sendMsg = form.SendData.value;	
-      
 
-    			
+
+
       if(sendMsg == "REQ_STOP")
       {	
 
         sendbuf = make_send_data(sendMsg);	
-            
-        
+
+
    console.log("333")
         $.ajax
           ({ url         : "http://127.0.0.1:9189"    
@@ -1148,10 +1148,10 @@ var iFlag = '0';
            , data        : encodeURI(sendbuf)
            , success     : function(data) {							
                form.RecvData.value = data;	
-           
+
            }
         });			 
-          
+
       }
       else 
       {		
@@ -1160,11 +1160,11 @@ var iFlag = '0';
         {
 
           console.log("2")
-   
+
           sendbuf = make_send_data(sendMsg);
-         
+
           iFlag = '1';
-          
+
           $.ajax
             ({ url         : "http://127.0.0.1:9188"    
              , type        : "POST"
@@ -1173,30 +1173,30 @@ var iFlag = '0';
              , success     : function(data) {							
               form.RecvData.value = data;							
               iFlag = '0';
-       
+
               //  if(data.length <= 506) { return alert("결제가 정상적으로 이루워지 않았습니다.")}
-              
-                
+
+
               const arr1 = form.RecvData.value.split("",60);
               const arr2 = sendbuf.split("",30)
-         
+
               console.log(arr1)
-              
+
          if(arr1[16].length >=10) { alert(arr1[16])}
-            
+
              const req = {
             phon: arr2[19],
             index:arr2[20]
 
 
              }  
-             
+
               fetch("/paymentHistory", {
                 method: "POST",
                 headers : {
                   "Content-Type" :"application/json"
                 },
-                
+
                  body: JSON.stringify(req),
                 })
                 .then((res => res.json()))
@@ -1206,14 +1206,14 @@ var iFlag = '0';
 
                   // location = "/login"
                 })
-         
+
              }
              , error       : function(request, status, error) {
               console.log(error )
               if(sendMsg == "RESTART" || sendMsg == "NVCATSHUTDOWN")
 
               {
-                
+
                 //응답 받지 않아서 예외 처리
               }
               else
@@ -1226,11 +1226,11 @@ var iFlag = '0';
         else	
           alert('버튼 중복 클릭');				
       }
-    		
-             
+
+
   }			
-  
-  
+
+
   function make_send_data(senddata) {
     var m_sendmsg;
     var m_totlen;
@@ -1240,12 +1240,12 @@ var iFlag = '0';
     m_totlen = 12 + m_bodylen;
 
     return NCpad(m_totlen,4) + "VCAT    " + NCpad(m_bodylen,4) + senddata;
-    
+
   }
 
   String.prototype.NCbyteLength = function(){
     var l=0;
-    
+
     for(var idx = 0; idx < this.length; idx++){
       var c = escape(this.charAt(idx));
 
@@ -1253,11 +1253,11 @@ var iFlag = '0';
       else if(c.indexOf("%u") != -1) l += 3;
       else if(c.indexOf("%") != -1) l += c.length/3; //JDK20210427 : UTF-8기준. EUC-KR은 /2로 수정 필요.
     }
-    
+
     return l;
   };
 
-  
+
   function NCpad(n,width)
   {
     n = n + '';
